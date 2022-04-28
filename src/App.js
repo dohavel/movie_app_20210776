f9feb229b3d78d6f76bfc299d2f728b0023be33e
dohavel <101759995+dohavel@users.noreply.github.com>
2022-04-28 23:30:18 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+function Sports({name,picture}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <h2>introduce {name}</h2>;
+    <img src={picture} />
     </div>
   );
 }
-
+const SportsILike =[
+  {
+    name:'Son',
+    image:'http://res.heraldm.com/content/image/2019/06/14/20190614000621_0.jpg',
+  },
+  {
+    name:'Salah',
+    image:'https://cdnweb01.wikitree.co.kr/webdata/editor/201803/26/img_20180326120937_432b3a9a.jpg',
+  },
+  {
+    name:'Harry Kane',
+    image:'https://ext.fmkorea.com/files/attach/new/20150714/1121272/69391174/192404380/34cd7e57be394b4d7f26d0cc13e30dc1.jpg',
+  },
+];
+function App() {
+  return (
+    <div>
+      {SportsILike.map(dish =>(<Sports name={dish.name} picture={dish.image}
+      />))}
+    </div>
+  );
+}
 export default App;
